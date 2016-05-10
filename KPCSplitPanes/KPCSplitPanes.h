4854d5e428b405f5dee66076c2b0c145e62c8598
Cédric <cedric@onekilopars.ec>
2016-05-10 15:23:28 +0200

    Initial Commit

---
+++ KPCSplitPanes/KPCSplitPanes.h
@@ -0,0 +1,19 @@
+//
+//  KPCSplitPanes.h
+//  KPCSplitPanes
+//
+//  Created by Cédric Foellmi on 10/05/16.
+//  Copyright © 2016 onekiloparsec. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KPCSplitPanes.
+FOUNDATION_EXPORT double KPCSplitPanesVersionNumber;
+
+//! Project version string for KPCSplitPanes.
+FOUNDATION_EXPORT const unsigned char KPCSplitPanesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KPCSplitPanes/PublicHeader.h>
+
+
