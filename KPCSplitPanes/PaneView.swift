57b65da67d4337899cb2f3883001662f404fdbbc
Cédric <cedric@onekilopars.ec>
2016-05-11 22:48:44 +0200

    Better that way

--- KPCSplitPanes/PaneView.swift
+++ KPCSplitPanes/PaneView.swift
@@ -13,22 +13,40 @@ class PaneView : NSView {
     @IBOutlet weak var closeButton: NSButton?
     @IBOutlet weak var splitButton: NSButton?
     
-    var paneTag : Int = -1
+    func parentSplitView() -> PressureSplitView? {
+        var view: NSView? = self
+        while view != nil && view?.isKindOfClass(PressureSplitView) == false {
+            view = view!.superview
+        }
+        return view as! PressureSplitView?
+    }
     
     override func removeFromSuperview() {
-        if self.superview?.isKindOfClass(PressureSplitView) == true {
-            let sv = self.superview as! PressureSplitView
-            sv.removeSubview(self)
-        }
+        self.parentSplitView()?.removeSubview(self)
     }
     
     override func viewDidMoveToWindow() {
         super.viewDidMoveToWindow()
+        
         if self.closeButton?.image == nil {
             self.closeButton?.image = PressureSplitView.defaultCloseIcon()
         }
         if self.splitButton?.image == nil {
             self.splitButton?.image = PressureSplitView.defaultSplitIcon()
         }
+        
+        self.closeButton?.target = self
+        self.closeButton?.action = #selector(PaneView.closePane)
+        
+        self.splitButton?.target = self
+        self.splitButton?.action = #selector(PaneView.splitPane)
+    }
+    
+    func closePane() {
+        self.parentSplitView()?.close(self)
+    }
+
+    func splitPane() {
+        self.parentSplitView()?.split(self)
     }
 }
