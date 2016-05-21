20e5e5a67648f18f967fe7882094b2bc4f94f383
Cédric <cedric@onekilopars.ec>
2016-05-21 21:00:04 +0200

    Cleaning up PaneView, making it public

--- KPCSplitPanes/PaneView.swift
+++ KPCSplitPanes/PaneView.swift
@@ -8,13 +8,11 @@
 
 import AppKit
 
-class PaneView : NSView {
+public class PaneView : NSView {
     
     @IBOutlet weak var closeButton: NSButton?
     @IBOutlet weak var splitButton: NSButton?
     
-    private var trackingArea: NSTrackingArea?
-
     static func newPaneView() -> PaneView {
         var topLevels = NSArray?()
         NSBundle(forClass: self).loadNibNamed("PaneView", owner: self, topLevelObjects: &topLevels)
@@ -25,63 +23,16 @@ class PaneView : NSView {
         return pv
     }
     
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        self.setup()
-    }
-    
-    override init(frame frameRect: NSRect) {
-        super.init(frame: frameRect)
-        self.setup()
-    }
-    
-    func setup() {
-        self.trackingArea = NSTrackingArea(rect: self.bounds,
-                                           options: [.ActiveInActiveApp, .MouseEnteredAndExited],
-                                           owner: self,
-                                           userInfo: nil)
-    }
-    
-    override var acceptsFirstResponder: Bool {
+    override public var acceptsFirstResponder: Bool {
         return true
     }
 
-    override class func requiresConstraintBasedLayout() -> Bool {
+    override public class func requiresConstraintBasedLayout() -> Bool {
         return false
     }
-
-//    // MARK: - Tracking
-//    
-//    override func updateTrackingAreas() {
-//        super.updateTrackingAreas()
-//        
-//        self.removeTrackingArea(self.trackingArea!)
-//        
-//        self.trackingArea = NSTrackingArea(rect: self.bounds,
-//                                           options: [.ActiveInActiveApp, .MouseMoved],
-//                                           owner: self,
-//                                           userInfo: nil)
-//        
-//        self.addTrackingArea(self.trackingArea!)
-//    }
-    
-    func parentSplitView() -> PressureSplitView? {
-        var view: NSView? = self
-        while view != nil && view?.isKindOfClass(PressureSplitView) == false {
-            view = view!.superview
-        }
-        return view as! PressureSplitView?
-    }
-    
-    override func removeFromSuperview() {
-        super.removeFromSuperview()
-        self.parentSplitView()?.removeArrangedSubview(self)
-    }
-    
-    override func viewDidMoveToWindow() {
-        super.viewDidMoveToWindow()
         
-        self.updateTrackingAreas()
+    override public func viewDidMoveToWindow() {
+        super.viewDidMoveToWindow()
         
         self.closeButton?.image = PaneView.closeIcon(false)
         self.splitButton?.image = PaneView.splitIcon(self.parentSplitView()?.useHorizontalSplitAsDefault==true, selected: false)
@@ -106,13 +57,23 @@ class PaneView : NSView {
         self.splitButton?.image = PaneView.splitIcon(self.parentSplitView()?.useHorizontalSplitAsDefault==true, selected: flag)
     }
     
-    override func flagsChanged(theEvent: NSEvent) {
+    override public func flagsChanged(theEvent: NSEvent) {
         let optionsKey = NSEventModifierFlags(rawValue: theEvent.modifierFlags.rawValue & NSEventModifierFlags.AlternateKeyMask.rawValue)
         let useAlt = (optionsKey == .AlternateKeyMask)
         let defaultHorizontal = self.parentSplitView()?.useHorizontalSplitAsDefault == true
         let horizontal = (useAlt == false && defaultHorizontal == true) || (useAlt == true && defaultHorizontal == false)
         let selected = (self.parentSplitView()?.selectedPaneView == self)
         self.splitButton?.image = PaneView.splitIcon(horizontal, selected: selected)
+    }
+    
+    // MARK: Helpers
+    
+    func parentSplitView() -> PressureSplitView? {
+        var view: NSView? = self
+        while view != nil && view?.isKindOfClass(PressureSplitView) == false {
+            view = view!.superview
+        }
+        return view as! PressureSplitView?
     }
     
     // MARK: - Icons (some troubles using NSBundle(forClass:) in an extension)
@@ -125,19 +86,19 @@ class PaneView : NSView {
         return img
     }
     
-    static func closeIcon(selected: Bool) -> NSImage {
+    static public func closeIcon(selected: Bool) -> NSImage {
         return PaneView.icon(named: "Close", selected: selected)
     }
     
-    static func splitHorizontalIcon(selected: Bool) -> NSImage {
+    static public func splitHorizontalIcon(selected: Bool) -> NSImage {
         return PaneView.icon(named: "SplitHorizontal", selected: selected)
     }
     
-    static func splitVerticalIcon(selected: Bool) -> NSImage {
+    static public func splitVerticalIcon(selected: Bool) -> NSImage {
         return PaneView.icon(named: "SplitVertical", selected: selected)
     }
     
-    static func splitIcon(horizontal: Bool, selected: Bool) -> NSImage {
+    static public func splitIcon(horizontal: Bool, selected: Bool) -> NSImage {
         return horizontal ? PaneView.splitHorizontalIcon(selected) : PaneView.splitVerticalIcon(selected)
     }
 }
